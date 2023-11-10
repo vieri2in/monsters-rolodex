ff586d7457bc56c6eee6c6a5508ada6f3d60ee67
BinLi <vieri21bin@gmail.com>
2023-11-09 22:11:35 -0800

    format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { Component} from 'react';
+import { Component } from 'react';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [{name:'jos'},{name:'app'}],
+      monsters: [{ name: 'jos' }, { name: 'app' }],
       searchString: ''
     };
     console.log("constructor");
@@ -13,36 +13,36 @@ class App extends Component {
   componentDidMount() {
     console.log("componentDidMount");
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.json())
-    // .then(users => {
-    //   this.setState(() => {
-    //     return {
-    //       monsters: users
-    //     }; 
-    //   });
-    // })
-    .then(users => {
-      this.setState({
+      .then((res) => res.json())
+      // .then(users => {
+      //   this.setState(() => {
+      //     return {
+      //       monsters: users
+      //     }; 
+      //   });
+      // })
+      .then(users => {
+        this.setState({
           monsters: users
         },
-        () => {
-          console.log(this.state);
-        });
-    })
-  //   .then(res => res.json())
-  //   .then(users => {
-  //     return {
-  //       monsters: users
-  //     }
-  //   },
-  //   () => {
-  //     console.log(this.state);
-  //   })
+          () => {
+            console.log(this.state);
+          });
+      })
+    //   .then(res => res.json())
+    //   .then(users => {
+    //     return {
+    //       monsters: users
+    //     }
+    //   },
+    //   () => {
+    //     console.log(this.state);
+    //   })
   }
   onSearchChange = (event) => {
     const searchString = event.target.value.toLowerCase();
     this.setState(() => {
-      return {searchString};
+      return { searchString };
     });
   }
   render() {
@@ -54,14 +54,14 @@ class App extends Component {
     });
     return (
       <div className="App">
-      <input 
-        className='search-box' 
-        type='search' 
-        placeholder='search monsters' 
-        onChange={onSearchChange} 
-      />
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={onSearchChange}
+        />
         {
-          filteredMonsters.map((m)=>{
+          filteredMonsters.map((m) => {
             return <div key={m.name}>
               <h1>
                 {m.name}
